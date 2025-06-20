f631bb5f55d16b917449b1646e9ca7ca0d1cfeb6
thivernale <thivernale@gmail.com>
2025-06-20 18:42:18 +0300

    Complete add/edit question pages

--- src/app/forum/questions/[id]/[slug]/edit/page.tsx
+++ src/app/forum/questions/[id]/[slug]/edit/page.tsx
@@ -1,13 +1,45 @@
-export default async function QuestionEditPage({
+'use client';
+
+import { ManageQuestion } from '@/components/ManageQuestion';
+import React, { useEffect, useState } from 'react';
+import { getQuestion } from '@/services/questionService';
+import { useAuthStore } from '@/store/Auth';
+import { Document, Question } from '@/services/types';
+import { useRouter } from 'next/navigation';
+import { slugify } from '@/utils/slugify';
+
+export default function QuestionEditPage({
   params,
 }: {
   params: Promise<{ id: string; slug: string }>;
 }) {
-  const { id, slug } = await params;
+  const { user } = useAuthStore();
+  const [question, setQuestion] = useState<Document<Question>>();
+  const router = useRouter();
+
+  useEffect(() => {
+    params
+      .then(({ id }) => getQuestion(id))
+      .then((result) => {
+        setQuestion(result);
+      })
+      .catch(() => {
+        router.push('/forum/questions');
+      });
+  }, [setQuestion, params, router]);
+
+  useEffect(() => {
+    if (question && (!user || user.$id !== question.authorId)) {
+      router.push(`/forum/questions/${question.$id}/${slugify(question.title)}`);
+    }
+  }, [question, user]);
+
+  if (!question || !user) return null;
 
   return (
-    <h1>
-      Question Edit Page {id}/{slug}
-    </h1>
+    <div className="container mx-auto flex flex-col px-4 pt-36 pb-20 xl:w-4xl">
+      ️<h1 className="mb-4 text-2xl font-bold">🏗️🚧👷‍♀ Edit Your Question </h1>
+      <ManageQuestion question={question} />
+    </div>
   );
 }
--- src/app/forum/questions/ask/page.tsx
+++ src/app/forum/questions/ask/page.tsx
@@ -1,3 +1,11 @@
+import { ManageQuestion } from '@/components/ManageQuestion';
+import React from 'react';
+
 export default async function QuestionAddPage() {
-  return <h1>Question Add Page</h1>;
+  return (
+    <div className="container mx-auto flex flex-col px-4 pt-36 pb-20 xl:w-4xl">
+      ️<h1 className="mb-4 text-2xl font-bold">🏗️🚧👷‍♀ Ask Your Question </h1>
+      <ManageQuestion />
+    </div>
+  );
 }
